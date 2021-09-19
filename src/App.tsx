534ae41f0f84944cb315475a2fce8458a626696f
t-takaaki <oden.tanaka38@gmail.com>
2021-09-19 13:52:02 +0900

    add license

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 // design from: https://www.bootdey.com/snippets/view/white-chat
+/*
+  Mit license
+  Copyright © 2021 bootdey.com
+  https://www.bootdey.com/license
+*/
 import "./App.css";
 import { ChatBody } from "./layouts/ChatBody";
 
